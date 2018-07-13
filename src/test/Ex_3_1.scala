fa51e5da696529dfe1f17e0efa5b4505afcf6689
김 영곤 <C0132@Cyworld.net>
2018-07-13 17:31:12 +0900

    first commit

---
+++ src/test/Ex_3_1.scala
@@ -0,0 +1,15 @@
+package test
+
+object Ex_3_1 {
+  def main(args: Array[String]): Unit = {
+    var x = 20
+    if (x > 15) {
+      println("15보다 큽니다.")
+      println("하하하")
+    } else if (x < 15) {
+      println("15보다 작습니다.")
+    } else {
+      println("과연 뭘까요?")
+    }
+  }
+}
